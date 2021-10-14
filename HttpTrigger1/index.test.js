dc76855f8fbfa5b003737b54d19777c4f03dbf30
Beytia <beytiamonge@gmail.com>
2021-10-13 21:29:04 -0500

    Version 1

---
+++ HttpTrigger1/index.test.js
@@ -0,0 +1,18 @@
+const httpFunction = require('./index');
+const context = require('../testing/Context');
+
+test('Http trigger example', async () => {
+    const request = {
+        query: { name: 'Norman Monge'}
+
+    };
+    
+    var interations = 10000;
+    console.time('FUNCTION #1');
+    for (var i = 0; i < interations; i++) {
+        await functions(context,request);
+    }
+    console.timeEnd('FUNCTION #1');
+    expect(context.res.body).toEqual('welcome, Norman');
+    expect(context.log.mock.calls.length).tobe(100000);
+});
